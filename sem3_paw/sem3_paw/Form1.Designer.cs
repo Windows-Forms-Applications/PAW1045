74f0238eb798de13f7372df9c0604ae93f7d6555
Cristian Ciurea <cristianciurea@gmail.com>
2020-03-04 12:42:41 +0200

    update

---
+++ sem3_paw/sem3_paw/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace sem3_paw
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbCod = new System.Windows.Forms.TextBox();
+            this.tbVarsta = new System.Windows.Forms.TextBox();
+            this.tbNume = new System.Windows.Forms.TextBox();
+            this.tbNote = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(103, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cod";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(103, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sex";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(103, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Varsta";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(103, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Nume";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(105, 217);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Note";
+            // 
+            // tbCod
+            // 
+            this.tbCod.Location = new System.Drawing.Point(176, 34);
+            this.tbCod.Name = "tbCod";
+            this.tbCod.Size = new System.Drawing.Size(100, 20);
+            this.tbCod.TabIndex = 5;
+            // 
+            // tbVarsta
+            // 
+            this.tbVarsta.Location = new System.Drawing.Point(176, 124);
+            this.tbVarsta.Name = "tbVarsta";
+            this.tbVarsta.Size = new System.Drawing.Size(100, 20);
+            this.tbVarsta.TabIndex = 6;
+            // 
+            // tbNume
+            // 
+            this.tbNume.Location = new System.Drawing.Point(176, 169);
+            this.tbNume.Name = "tbNume";
+            this.tbNume.Size = new System.Drawing.Size(100, 20);
+            this.tbNume.TabIndex = 7;
+            // 
+            // tbNote
+            // 
+            this.tbNote.Location = new System.Drawing.Point(176, 210);
+            this.tbNote.Name = "tbNote";
+            this.tbNote.Size = new System.Drawing.Size(100, 20);
+            this.tbNote.TabIndex = 8;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "M",
+            "F"});
+            this.comboBox1.Location = new System.Drawing.Point(176, 78);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 303);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.tbNote);
+            this.Controls.Add(this.tbNume);
+            this.Controls.Add(this.tbVarsta);
+            this.Controls.Add(this.tbCod);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbCod;
+        private System.Windows.Forms.TextBox tbVarsta;
+        private System.Windows.Forms.TextBox tbNume;
+        private System.Windows.Forms.TextBox tbNote;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
