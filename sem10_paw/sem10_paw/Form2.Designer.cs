51b95d2443d19a2c0a59c581733182975c0be856
Cristian Ciurea <cristianciurea@gmail.com>
2020-05-05 19:29:50 +0300

    update

---
+++ sem10_paw/sem10_paw/Form2.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace sem10_paw
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbNume = new System.Windows.Forms.TextBox();
+            this.tbVarsta = new System.Windows.Forms.TextBox();
+            this.tbAdresa = new System.Windows.Forms.TextBox();
+            this.cbSex = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbNume
+            // 
+            this.tbNume.Location = new System.Drawing.Point(160, 93);
+            this.tbNume.Name = "tbNume";
+            this.tbNume.Size = new System.Drawing.Size(100, 20);
+            this.tbNume.TabIndex = 0;
+            // 
+            // tbVarsta
+            // 
+            this.tbVarsta.Location = new System.Drawing.Point(160, 164);
+            this.tbVarsta.Name = "tbVarsta";
+            this.tbVarsta.Size = new System.Drawing.Size(100, 20);
+            this.tbVarsta.TabIndex = 1;
+            // 
+            // tbAdresa
+            // 
+            this.tbAdresa.Location = new System.Drawing.Point(160, 320);
+            this.tbAdresa.Multiline = true;
+            this.tbAdresa.Name = "tbAdresa";
+            this.tbAdresa.Size = new System.Drawing.Size(303, 83);
+            this.tbAdresa.TabIndex = 2;
+            // 
+            // cbSex
+            // 
+            this.cbSex.FormattingEnabled = true;
+            this.cbSex.Items.AddRange(new object[] {
+            "M",
+            "F"});
+            this.cbSex.Location = new System.Drawing.Point(160, 238);
+            this.cbSex.Name = "cbSex";
+            this.cbSex.Size = new System.Drawing.Size(121, 21);
+            this.cbSex.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(80, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nume";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(83, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Varsta";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(83, 238);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Sex";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(83, 332);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Adresa";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(170, 449);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Adauga";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 508);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbSex);
+            this.Controls.Add(this.tbAdresa);
+            this.Controls.Add(this.tbVarsta);
+            this.Controls.Add(this.tbNume);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbNume;
+        private System.Windows.Forms.TextBox tbVarsta;
+        private System.Windows.Forms.TextBox tbAdresa;
+        private System.Windows.Forms.ComboBox cbSex;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ sem10_paw/sem10_paw/Form2.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.OleDb;
+
+namespace sem10_paw
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OleDbConnection conexiune = new OleDbConnection("Provider = Microsoft.ACE.OLEDB.12.0; Data Source = student.accdb");
+            try
+            {
+                conexiune.Open();
+                OleDbCommand comanda = new OleDbCommand();
+                comanda.Connection = conexiune;
+
+                comanda.CommandText = "SELECT MAX(cod) FROM student";
+                int cod = Convert.ToInt32(comanda.ExecuteScalar());
+
+                comanda.CommandText = "INSERT INTO student VALUES(?,?,?,?,?)";
+                comanda.Parameters.Add("cod", OleDbType.Integer).Value = cod + 1;
+                comanda.Parameters.Add("nume", OleDbType.Char, 20).Value = tbNume.Text;
+                comanda.Parameters.Add("varsta", OleDbType.Integer).Value = Convert.ToInt32(tbVarsta.Text);
+                comanda.Parameters.Add("sex", OleDbType.Char, 2).Value = cbSex.Text;
+                comanda.Parameters.Add("adresa", OleDbType.Char, 30).Value = tbAdresa.Text;
+                comanda.ExecuteNonQuery();
+            }
+            catch (OleDbException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conexiune.Close();
+                tbNume.Clear();
+                tbVarsta.Clear();
+                tbAdresa.Clear();
+                cbSex.Text = "";
+            }
+        }
+    }
+}
